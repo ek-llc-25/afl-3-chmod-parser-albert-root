88f501c55e928109cc810543236261dbd9493a58
albertglyngbo <albertglyngbo@gmail.com>
2025-10-01 20:23:29 +0200

    Afleveringsopgave 3: chmod-parser

--- perms.c
+++ perms.c
@@ -9,31 +9,48 @@
 // - + og -
 // - r, w, x, rw, rx, wx, rwx (eller en anden rækkefølge af dem)
 bool chmod(const char *input, struct perms_t *perms) {
-    if (input == NULL) {
+    if (input == NULL || perms == NULL) {
+        return false;
+    } else if (input[0] != 'u' && input[0] != 'g' && input[0] != 'o' && input[0] != 'a') {
+        return false;
+    } else if (input[1] != '+' && input[1] != '-') {
+        return false;
+    } else if (input[2] == '\0') {
         return false;
     }
 
-    if (input[0] != 'o') {
-        return false;
+    unsigned int perm_bits = 0;
+    for (size_t i = 2; input[i] != '\0'; ++i) {
+        switch (input[i]) {
+            case 'r': perm_bits |= USER_READ;
+                break;
+            case 'w': perm_bits |= USER_WRITE;
+                break;
+            case 'x': perm_bits |= USER_EXECUTE;
+                break;
+            default: return false;
+        }
     }
 
-    if (input[1] != '-') {
-        return false;
+    switch (input[0]) {
+        case 'u':
+            break;
+        case 'g':
+            perm_bits >>= 3;
+            break;
+        case 'o':
+            perm_bits >>= 6;
+            break;
+        case 'a':
+            perm_bits = perm_bits | (perm_bits >> 3) | (perm_bits >> 6);
+            break;
     }
 
-    if (input[2] != 'r') {
-        return false;
+    if (input[1] == '+') {
+        perms->bits |= perm_bits;
+    } else {
+        perms->bits &= ~perm_bits;
     }
 
-    // Når man AND'er med en maske med 0'er i, slukker man for enkelte bits
-    // Når man OR'er med en maske med 1'er i, tænder man for enkelte bits
-
-    // Hvis OTHER_READ er 0b000'000'100
-    // så er ~OTHER_READ  0b111'111'011
-    //
-    // Eksempel:
-    // 0644 og o-r giver 0640
-    // og når man &'er med den maske, bliver den mindste bit sat til 0, og de resterende bits forbliver som de er.
-    perms->bits &= ~OTHER_READ;
     return true;
 }
